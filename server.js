3e251446452e3a309c709bda5e17fc1903adbc73
Sahil Kashyap <sahilkashyap64@gmail.com>
2021-05-29 19:02:23 +0530

    added websocket and index.html

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
-
+const path = require('path');
+const WebSocket = require('ws'); // new
 const app = express()
 app.use(express.json());
 app.use(express.urlencoded({
@@ -7,7 +8,7 @@ app.use(express.urlencoded({
 }));
 const port = 3000
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.sendFile(path.join(__dirname, 'index.html'));
 })
 app.post('/auth', function (req, res) {
     // res.send('Got a POST request')
@@ -22,3 +23,13 @@ app.post('/auth', function (req, res) {
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
+
+const socketServer = new WebSocket.Server({port: 3030});
+socketServer.on('connection', (socketClient) => {
+  console.log('connected');
+  console.log('client Set length: ', socketServer.clients.size);
+  socketClient.on('close', (socketClient) => {
+    console.log('closed');
+    console.log('Number of clients: ', socketServer.clients.size);
+  });
+});
