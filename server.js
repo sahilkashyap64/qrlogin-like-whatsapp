47081401b7da9f80408f4bb22c3fc65181d81680
Sahil Kashyap <sahilkashyap64@gmail.com>
2021-05-29 19:18:11 +0530

    Basic chatApp

--- server.js
+++ server.js
@@ -25,9 +25,20 @@ app.listen(port, () => {
 })
 
 const socketServer = new WebSocket.Server({port: 3030});
+const messages = ['Start Chatting!'];
+
 socketServer.on('connection', (socketClient) => {
   console.log('connected');
-  console.log('client Set length: ', socketServer.clients.size);
+  console.log('Number of clients: ', socketServer.clients.size);
+  socketClient.send(JSON.stringify(messages));
+  socketClient.on('message', (message) => {
+    messages.push(message);
+    socketServer.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(JSON.stringify([message]));
+      }
+    });
+  });
   socketClient.on('close', (socketClient) => {
     console.log('closed');
     console.log('Number of clients: ', socketServer.clients.size);
